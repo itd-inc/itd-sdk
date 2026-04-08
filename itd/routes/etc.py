341f1020207d5a21fcc9e4371d914c2e7fbb2990
firedotguy <nta16022013@gmail.com>
2026-04-08 21:14:52 +0300

    feat: add clans

--- itd/routes/etc.py
+++ itd/routes/etc.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from itd.exceptions import catch_errors
 if TYPE_CHECKING:
     from itd.client import Client
 
+catch_errors()
 def get_top_clans(client: Client):
     return client.request('get', 'users/stats/top-clans')
 
+catch_errors()
 def get_who_to_follow(client: Client):
     return client.request('get', 'users/suggestions/who-to-follow')
